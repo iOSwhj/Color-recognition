fb8f68c944b9869227cc083b86e659373f78919f
mengduan <151176184+iOSwhj@users.noreply.github.com>
2024-03-12 10:09:23 +0800

    Initial Commit

---
+++ coremlproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coremlproject
+//
+//  Created by Mengduan on 2024/3/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
